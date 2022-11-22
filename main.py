c9022f2fbad8cd1e744f253901a0d664945bd97c
ambrisolla <andremuzel@gmail.com>
2022-11-22 11:12:14 -0300

    Adding v0.1

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from lib.system.environments import ConfigEnvs
 from flask import Flask
-from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
 
 import routes.auth
 import routes.users
@@ -15,5 +13,5 @@ def hello():
   }
 
 if __name__ == '__main__':
-  ConfigEnvs() # load environments
+  ConfigEnvs()
   app.run(host='0.0.0.0', port=5000, debug=True)
